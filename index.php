0ee0a0abef5f6944ebaf2960e83d5aee9e463797
fastechnology <fastechnology99@gmail.com>
2019-05-01 16:35:41 +0000

    LOGIN_1

--- index.php
+++ index.php
@@ -5,11 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="Framework/bootstrap-4.1.3-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/login/login.css">
     <title>Pizzeria: Login</title>
 </head>
 <body>
-   <?php require 'componentes/navbarsetc/navbar.php' ?>
     <br>
     <div class="login">
         <h2>Inicio de Sesión:</h2>
