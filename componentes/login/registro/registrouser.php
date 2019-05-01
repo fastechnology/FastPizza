ff541188bfcc2a973562f40ce8511d95e155a519
fastechnology <fastechnology99@gmail.com>
2019-05-01 17:14:04 +0000

    BUG_2

--- componentes/login/registro/registrouser.php
+++ componentes/login/registro/registrouser.php
@@ -6,10 +6,5 @@ $email=$_POST['email'];
 $tipo=2;
 $sql=$conexion->query("INSERT into clientes (usuario_cliente,email_cliente,pass_cliente,tipo_user) values
  ('$user','$email','$pass',$tipo)");
-if($sql){
-    session_start();
-    $_SESSION['usuarioactual']=$user;
-    $_SESSION['emailuseractual']=$email;
 header("Location: ../../home/completardatospedido/index.php");
-}
 ?>
