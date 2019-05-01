6d14b589c860d6f7fa1d10efaa40cb3df1d9d351
fastechnology <fastechnology99@gmail.com>
2019-05-01 17:23:29 +0000

    REGISTRO_1

--- componentes/login/registro/registrouser.php
+++ componentes/login/registro/registrouser.php
@@ -6,7 +6,10 @@ $email=$_POST['email'];
 $tipo=2;
 $sql=$conexion->query("INSERT into clientes (usuario_cliente,email_cliente,pass_cliente,tipo_user) values
  ('$user','$email','$pass',$tipo)");
- if($sql){
-echo "OK";
- }
+if($sql){
+    session_start();
+    $_SESSION['usuarioactual']=$user;
+    $_SESSION['emailuseractual']=$email;
+header("Location: ../../home/completardatospedido/index.php");
+}
 ?>
