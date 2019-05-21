6a1503c6d1e06f19b995befc95d944bc91b3d6d4
HOODY\HP <HoodyB>
2019-05-21 15:50:44 -0500

    Menú pizzas

--- componentes/home/home/pizzas.php
+++ componentes/home/home/pizzas.php
@@ -4,13 +4,96 @@ require ('../../navbar/index.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="../../../css/pizzas.css">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title>Document</title>
+ <title></title>
 </head>
+
 <body>
- PIZZASSSSSSSSSSSSSSSS
+
+ <header>
+  <div class="textos">
+   <h1 class="titulo">Star-Soft</h1>
+   <h3 class="subtitulo">Dessarrollo Web Pizzeria </h3>
+   <a href="index.php" class="boton">Home</a>
+  </div>
+  
+ </header>
+ <main>
+  <section class="acerca-de">
+   <div class="wrap">
+    <div class="tarjeta-wrap">
+     <div class="tarjeta">
+         <div class="adelante img1"></div>
+          <div class="atras">
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sed!</p>
+          </div>
+     </div>
+     <a href="#" class="boton">Ordenar</a>
+    </div>
+   
+    <div class="tarjeta-wrap">
+     <div class="tarjeta">
+         <div class="adelante img2"></div>
+          <div class="atras">
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sed!</p>
+          </div>
+     </div>
+     <a href="#" class="boton">Ordenar</a>
+    </div>
+   
+    <div class="tarjeta-wrap">
+     <div class="tarjeta">
+         <div class="adelante img3"></div>
+          <div class="atras">
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sed!</p>
+          </div>
+     </div>
+     <a href="#" class="boton">Ordenar</a>
+    </div> 
+   </div>
+  </section>
+
+  <section class="acerca-de">
+   <div class="wrap">
+    <div class="tarjeta-wrap">
+     <div class="tarjeta">
+         <div class="adelante img4"></div>
+          <div class="atras">
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sed!</p>
+          </div>
+     </div>
+     <a href="#" class="boton">Ordenar</a>
+    </div>
+    
+    <div class="tarjeta-wrap">
+     <div class="tarjeta">
+         <div class="adelante img5"></div>
+          <div class="atras">
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sed!</p>
+          </div>
+     </div>
+     <a href="#" class="boton">Ordenar</a>
+    </div>
+    
+    <div class="tarjeta-wrap">
+     <div class="tarjeta">
+         <div class="adelante img6"></div>
+          <div class="atras">
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sed!</p>
+          </div>
+     </div>
+     <a href="#" class="boton">Ordenar</a>
+    </div>
+   </div>
+  </section>
+
+  <section class="fondo">
+  </section>
+  </main>
 </body>
 </html>
