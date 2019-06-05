c37f540cce788baffb76dbb905a439c997e5935e
Besiddes <Besiddes1@gmail.com>
2019-06-05 13:44:04 -0500

    admin_prueba_2

--- componentes/home/adminhome/navar/navar.php
+++ componentes/home/adminhome/navar/navar.php
@@ -17,14 +17,14 @@
       <div class="collapse navbar-collapse" id="collapsibleNavId">
           <ul class="navbar-nav mr-5 mt-0 mt-lg-0">
               <li class="nav-item active mr-5">
-                  <a class="nav-link" href="http://localhost/FastPizza/componentes/home/adminhome/adminhome.php">     Inicio <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="../adminhome.php">     Inicio <span class="sr-only">(current)</span></a>
               </li>
              
               <li class="nav-item dropdown mr-5">
                   <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" 
                   aria-expanded="false">    Cliente</a>
                   <div class="dropdown-menu" aria-labelledby="dropdownId">
-                      <a  class="dropdown-item" href="http://http://app-89ebffe6-07c2-4e7e-8eb3-00a845019f47.cleverapps.io/FastPizza/componentes/home/adminhome/MenuAdmin/listaCliente.php">Lista clientes</a>
+                      <a  class="dropdown-item" href="../MenuAdmin/listaCliente.php">Lista clientes</a>
                       <a  class="dropdown-item" style="cursor: pointer" data-toggle="modal" data-target="#addusuarios">Agregar Cliente</a>
                   </div>
               </li>
@@ -32,7 +32,7 @@
                   <a class="nav-link dropdown-toggle " href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" 
                   aria-expanded="false">    Pizzas</a>
                   <div class="dropdown-menu" aria-labelledby="dropdownId">
-                      <a  class="dropdown-item" href="http://http://app-89ebffe6-07c2-4e7e-8eb3-00a845019f47.cleverapps.io/FastPizza/componentes/home/adminhome/MenuAdmin/listaPizza.php">Lista Pizzas</a>
+                      <a  class="dropdown-item" href="../MenuAdmin/listaPizza.php">Lista Pizzas</a>
                       <a  class="dropdown-item" style="cursor: pointer" data-toggle="modal" data-target="#addpizzas">Agregar Pizza</a>
                   </div>
               </li>
@@ -40,7 +40,7 @@
                   <a class="nav-link dropdown-toggle " href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" 
                   aria-expanded="false">    Ventas</a>
                   <div class="dropdown-menu" aria-labelledby="dropdownId">
-                      <a class="dropdown-item" href="http://http://app-89ebffe6-07c2-4e7e-8eb3-00a845019f47.cleverapps.io/FastPizza/componentes/home/adminhome/MenuAdmin/listaVenta.php" >Lista Ventas</a>
+                      <a class="dropdown-item" href="../MenuAdmin/listaVenta.php" >Lista Ventas</a>
                       <!--<a class="dropdown-item" href="../Vista/login/Registro.php">Agregar Venta</a>-->
                   </div>
               </li>
@@ -48,7 +48,7 @@
               
           </ul>
           <li class="nav-item form-inline my-1 my-lg-1">
-                  <a class="nav-link " href="../../sesiones/cerrarsesion.php">Cerrar Sesion</a>
+                  <a class="nav-link " href="../../../sesiones/cerrarsesion.php">Cerrar Sesion</a>
               </li>
          <!-- <form class="form-inline my-2 my-lg-0">
               <input class="form-control mr-sm-2" type="text" placeholder="Search">
