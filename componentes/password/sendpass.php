23195b1b0df5d83e79964e9cb8c26fbc32881bfc
Besiddes <Besiddes1@gmail.com>
2019-06-07 08:57:08 -0500

    gg_yaplisxd

--- componentes/password/sendpass.php
+++ componentes/password/sendpass.php
@@ -1,60 +1,4 @@
 <?php
-
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require '../../basededatos/conexion.php';
-require 'mailer/Exception.php';
-require 'mailer/PHPMailer.php';
-require 'mailer/SMTP.php';
-
-$email=$_POST['email'];
-$pass=$_POST['pass2'];
-$tipo=2;
-$nombre=$_POST['name'];
-$direccion=$_POST['direccion'];
-$phone=$_POST['phone'];
-$activacion=1;
-
-
-        header("Location: ../errorregistro.php");
-    }
-    else{
-        $sql2=$conexion->query("INSERT into clientes values (null,'$nombre','$direccion',$phone,'$email','$pass',$tipo,$activacion)");
-if($sql2){
-    $mail = new PHPMailer(true);
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-    $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'fastechnology99@gmail.com';                     // SMTP username
-    $mail->Password   = 'Tecnologia2019';                               // SMTP password
-    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->setFrom('fastechnology99@gmail.com', 'Star-Soft');
-    $mail->addAddress($email);     // Add a recipient
-
-    // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Star-Soft Registro.';
-    $mail->Body    = 'Te haz registrado exitosamente en Star-Soft. Tus datos estan seguros con nosotros :D. Tus datos para iniciar sesion son: Email: '$email' y Contraseña: '$pass'';
-    $mail->send();
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-header("Location: ../../../index.php");
-} 
-    echo "No se ha podido conectar con el servidor, ".'<a href="../../../index.php">Reintentar</a>';   }
-    ?>
-    <?php
 require '../basededatos/conexion.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -62,9 +6,6 @@ require '../login/registro/mailer/Exception.php';
 require '../login/registro/mailer/PHPMailer.php';
 require '../login/registro/mailer/SMTP.php';
 $email=$_POST['email'];
-$sql1=$conexion->query("SELECT *from clientes where (email_cliente='$email') ");
-    $filas=$sql1->num_rows;
-    if($filas>0){
 $sql=$conexion->query("SELECT pass_cliente from clientes where (email_cliente='$email') ");
 $filas=$sql->num_rows;
 if($sql){
@@ -91,7 +32,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Star-Soft: Recuperacion contraseña.';
-    $mail->Body    = 'Tus datos para iniciar sesion en: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/ 
+    $mail->Body    = 'Tus datos para iniciar sesion en: http://app-89ebffe6-07c2-4e7e-8eb3-00a845019f47.cleverapps.io/ 
     son los siguientes: '. 'Email: ' . $email . 'Contraseña: ' . $pass;
     $mail->send();
 } catch (Exception $e) {
@@ -99,7 +40,6 @@ try {
 }
 header("Location: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/");
 }
-    }
-    else{
-        header("Location: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/componentes/password/resetpassword.php");
-    }
+else{
+    header("Location: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/componentes/password/resetpassword.php");
+}
