1c0cbfe64fc118cdb2f08fab272d5c8f170d726a
Besiddes <Besiddes1@gmail.com>
2019-06-07 10:28:20 -0500

    Ssistema_registro

--- componentes/login/registro/registro.php
+++ componentes/login/registro/registro.php
@@ -1,1 +1,19 @@
 prueba <a href="www.google.com">Ir</a>
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require '../../basededatos/conexion.php';
+require 'mailer/Exception.php';
+require 'mailer/PHPMailer.php';
+require 'mailer/SMTP.php';
+
+$email=$_POST['email'];
+$pass=$_POST['pass2'];
+$tipo=2;
+$nombre=$_POST['name'];
+$direccion=$_POST['direccion'];
+$phone=$_POST['phone'];
+$activacion=1;
+
+echo $nombre;
+?>
