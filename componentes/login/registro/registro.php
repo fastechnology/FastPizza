eaab3a7523406233c839ea58336e8f4b917b1c13
Besiddes <Besiddes1@gmail.com>
2019-06-07 10:11:30 -0500

    creoqueyaxd:D

--- componentes/login/registro/registro.php
+++ componentes/login/registro/registro.php
@@ -14,9 +14,46 @@ $direccion=$_POST['direccion'];
 $phone=$_POST['phone'];
 $activacion=1;
 
-echo $email;
-echo $pass;
-echo $nombre;
-echo $direccion;
-echo $phone;
+$sql=$conexion->query("SELECT *from clientes where (email_cliente='$email') ");
+    $filas=$sql->num_rows;
+    if($filas>0){
+        header("Location: ../errorregistro.php");
+    }
+    else{
+        $sql2=$conexion->query("INSERT into clientes (nombre_cliente,direccion_cliente,telefono_cliente,email_cliente,pass_cliente,tipo_user,activacion) values ('$nombre','$direccion',$phone,'$email','$pass',$tipo,$activacion)");
+if($sql2){
+    $mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'fastechnology99@gmail.com';                     // SMTP username
+    $mail->Password   = 'Tecnologia2019';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('fastechnology99@gmail.com', 'Star-Soft');
+    $mail->addAddress($email);     // Add a recipient
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Star-Soft Registro.';
+    $mail->Body    = 'Gracias por registrarte';
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+header("Location: ../../../index.php");
+}
+else { 
+    echo "No se ha podido conectar con el servidor, ".'<a href="../../../index.php">Reintentar</a>';   }
+    ?>
+    
 ?>
