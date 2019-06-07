491b63822ec5f57af6d1a88c3333c42ad51d8bec
Besiddes <Besiddes1@gmail.com>
2019-06-07 01:19:40 -0500

    sesion_prueba

--- componentes/home/home/menu/index.php
+++ componentes/home/home/menu/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
  <meta charset="UTF-8">
- <?php require '../../../sesiones/sesionhome.php'; ?>
+ 
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="stylesheet" href="../../../../css/home.css">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
