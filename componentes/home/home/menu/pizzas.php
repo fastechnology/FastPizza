8cb4bc15ac22aad38da6479f413414dbafab35f8
Besiddes <Besiddes1@gmail.com>
2019-06-07 01:45:53 -0500

    sesiones_2.0

--- componentes/home/home/menu/pizzas.php
+++ componentes/home/home/menu/pizzas.php
@@ -2,7 +2,7 @@
     <html lang="en">
     <head>
     <meta charset="UTF-8">
-    
+    <?php session_start(); if($_SESSION['user']!=""){ ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../../css/menup.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -64,3 +64,7 @@
 <br>
 </body>
 </html>
+<?php }
+        else{
+            header("Location: ../../../../index.php");
+        }
