416db78f70a2b64367d865ba13201c05704d0644
Besiddes <Besiddes1@gmail.com>
2019-06-07 01:22:48 -0500

    sesion_login

--- componentes/sesiones/sesionlogin.php
+++ componentes/sesiones/sesionlogin.php
@@ -3,7 +3,8 @@ error_reporting(0);
 session_start();
 $session=$_SESSION['user'];
 if ($session==""){
-    header("Location: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/");
+    header("Location: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/index.php");
+    
 }
 else{
  header("Location: http://app-26e39479-bc52-47d9-bd0f-14ddb6d466c3.cleverapps.io/componentes/home/home/menu/index.php");   
