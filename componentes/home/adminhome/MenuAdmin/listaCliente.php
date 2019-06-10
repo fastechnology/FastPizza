c62384e46a9e07a24bce74dcbcd9faa8b81669c1
tirandoCodigoF <44149781+tirandoCodigoF@users.noreply.github.com>
2019-06-09 21:25:01 -0500

    xd

--- componentes/home/adminhome/MenuAdmin/listaCliente.php
+++ componentes/home/adminhome/MenuAdmin/listaCliente.php
@@ -18,11 +18,11 @@ include '../adminhome.php';
   </style> -->
 
   <div class="container">
-      <br>
-   
-     
+  <div class="row d-flex justify-content-around col-md-offset-1 " >
+      
+      </div>
         <div class="row d-flex justify-content-around mt-5">
-            <div class="card col-md-12 col-md-offset-1 bg-light">
+            <div class="card col-md-15 col-md-offset-1 bg-light">
            
                 <article class="card-body">
                 <h2 align="center">Lista Clientes</h2>
@@ -30,7 +30,7 @@ include '../adminhome.php';
                 <!--<button type="submit" class="btn btn-info btn-xs" data-toggle="modal" data-target="#ActulizarVenta">Activar Usuario</button>                             
 --><div class="row">
 		<div id="cuadro1" class="col-sm-12 col-md-12 col-lg-12">
-			<div class="col-sm-offset-2 col-sm-8 ">
+			<div class="col-sm-offset-1 col-sm-8 ">
 				<h3 class="text-center"> <small class="mensaje"></small></h3>
 			</div>
 			<div class="table-responsive col-sm-12">		
