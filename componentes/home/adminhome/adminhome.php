5c7c9c71c717db54398decb0fa9404afc7a11162
tirandoCodigoF <44149781+tirandoCodigoF@users.noreply.github.com>
2019-06-09 20:40:53 -0500

    XD

--- componentes/home/adminhome/adminhome.php
+++ componentes/home/adminhome/adminhome.php
@@ -1,8 +1,7 @@
 <?php 
     include_once 'navar/head.php';
     include_once 'navar/navar.php';
-   // include_once 'MenuAdmin/modaladd.php';
-   // include_once 'MenuAdmin/modalpizza.php';
+   
  ?>
 
 
@@ -10,22 +9,15 @@
 <html lang="es">
 <style>
   body{
+    
   background-image: url(imGP/15.jpg);
   background-repeat: no-repeat;
-  background-position: relative;
+  background-position: center;
   background-size: cover;
 
   }
   </style>
 <body >
-    <br>
-        <br>
-   
-   
+ 
 </body>
-<?php //include_once 'navar/footer.php';     
-    ?>
-     <!-- <script src="./modelo/datolista.js"></script>
-    <script src="./modelo/UsuarioDao.js"></script>
-    <script src="./modelo/datapizza.js"></script> -->
 </html>
